3b310bc561ca98a37601e9754805956f8fe83544
Richard Guo <richardguo0122@gmail.com>
2016-12-04 16:56:28 -0500

    updated regression model

--- ML/Stock_ML.py
+++ ML/Stock_ML.py
@@ -5,10 +5,13 @@ warnings.warn = warn
 
 import pandas as pd
 import numpy as np
-from sklearn import preprocessing, cross_validation, neighbors, svm
-from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing, cross_validation
+from sklearn.linear_model import LinearRegression, LogisticRegression, BayesianRidge
+from sklearn.ensemble import GradientBoostingRegressor, AdaBoostRegressor
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neural_network import MLPRegressor
 import glob, os
-import datetime
 import cPickle as pk
 
 def ML (dir,file):
@@ -21,31 +24,33 @@ def ML (dir,file):
 
     X = np.array(df.drop(['priceChange_y'],1))
     X = preprocessing.scale(X)
+    df['priceChange_y'] = df['priceChange_y'].map(lambda x: x * 100) #multiply float to make percentage
     y = np.array(df['priceChange_y'])
+    y = preprocessing.scale(y)
 
     #X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size= 0.2)
 
     #Model training
-    clf = LinearRegression()
+    clf = BayesianRidge()
+    clf.fit(X, y.astype(int)) #change float lable to int
     #save Model
     with open('stocks.p','wb') as f:
         pk.dump(clf,f)
     clf = pk.load(open('stocks.p','rb'))
-    clf.fit(X, y)
 
-    #print clf.score(X_test,y_test)
+    #print clf.score(X_test,y_test.astype(int)) #change float lable to int
 
     #New data
     df = pd.read_csv(dir + file)
     df = df.drop(df[df.EPS < 0].index) #removes stocks with EPS<0
     df.replace('NaN',0,inplace = True)
 
-    X_test = np.array(df)
+    X_predict = np.array(df)
 
     Output = {}
 
     #Output
-    for i in X_test:
+    for i in X_predict:
         try:
             k = np.delete(i,0)
             prediction = clf.predict(k)
@@ -69,17 +74,17 @@ if __name__ == '__main__':
     file_list =  sorted(file_list,reverse = True)
 
     for i in range(len(file_list)-3): #iterate through all available VPN files
-        #Vlooking up today's price change to yesterday's price change
-        df_today = pd.read_csv(data_dir + '%s' %file_list[i])[['Ticker&Date','priceChange']]
-        df_today['Ticker&Date'] = df_today['Ticker&Date'].map(lambda x: x.lstrip('+-').rstrip('0123456789'))
-        df_yes = pd.read_csv(data_dir + '%s' %file_list[i+1])
-        df_yes['Ticker&Date'] = df_yes['Ticker&Date'].map(lambda x: x.lstrip('+-').rstrip('0123456789'))
-        learning_df =  df_yes.merge(df_today, on = 'Ticker&Date', how = 'left')
-        learning_df['Ticker&Date'] = learning_df['Ticker&Date'] + file_list[i][-14:-4].replace('-','') #adding back timestamp
-        '''learning_df.set_index('Ticker&Date').to_csv(data_dir +'LearningFile.csv')'''
-        #appending learning file to master learning file
-        VPNOutput = pd.read_csv(data_dir +'LearningFile.csv')
-        outputF =  VPNOutput.append(learning_df).drop_duplicates().set_index('Ticker&Date')
-        outputF.to_csv(data_dir +'LearningFile.csv')
+        if int(file_list[i][-6:-4]) - int(file_list[i+1][-6:-4]) in (1,3): #Vlooking up today's price change to yesterday's price change
+            df_today = pd.read_csv(data_dir + '%s' %file_list[i])[['Ticker&Date','priceChange']]
+            df_today['Ticker&Date'] = df_today['Ticker&Date'].map(lambda x: x.lstrip('+-').rstrip('0123456789'))
+            df_yes = pd.read_csv(data_dir + '%s' %file_list[i+1])
+            df_yes['Ticker&Date'] = df_yes['Ticker&Date'].map(lambda x: x.lstrip('+-').rstrip('0123456789'))
+            learning_df =  df_yes.merge(df_today, on = 'Ticker&Date', how = 'left')
+            learning_df['Ticker&Date'] = learning_df['Ticker&Date'] + file_list[i][-14:-4].replace('-','') #adding back timestamp
+            '''learning_df.set_index('Ticker&Date').to_csv(data_dir +'LearningFile.csv')'''
+            #appending learning file to master learning file
+            VPNOutput = pd.read_csv(data_dir +'LearningFile.csv')
+            outputF =  VPNOutput.append(learning_df).drop_duplicates().set_index('Ticker&Date')
+            outputF.to_csv(data_dir +'LearningFile.csv')
 
     ML(data_dir,file_list[0])
